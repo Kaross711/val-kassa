ae58c55b84b7f72b08ae69a01b96e7063246f033
Karos Abdulqadir <karos.abdulqadir@hotmail.com>
2026-03-05 16:42:10 +0100

    Update: kassa en voorraad in 1 gevoegd

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: "standalone",
     eslint: { ignoreDuringBuilds: true }, // ← blokkeert build niet meer op ESLint
     // typescript: { ignoreBuildErrors: true }, // ← alleen inschakelen als TS ook zou blokkeren
 };
